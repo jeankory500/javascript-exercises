3be3ec6ec8a6d3355f02b5992dfdf59f27fff6c4
JeankoryV <124745650+jeankory500@users.noreply.github.com>
2025-11-18 22:36:04 -0500

    firstDay COMP

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -1,23 +1,23 @@
 /*
-firstName: "Carlos"
-lastName: "Stevenson"
-thisYear: 1965
-birthYear: 1947
-
+ First name: Carlos
+ Last name: Stevenson
+ This year: 1965
+ Birth year: 1947
 
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
-const { firstName, lastName, birthYear } = require("./joinStrings-example")
+const { firstName, lastName, birthYear, thisYear } = require("./joinStrings-example")
 
 
 // Add your code right below, good luck!
-const fullName = firstName + " " + lastName;
-const age = thisYear - birthYear;
+const fullName = (firstName + " " + lastName);
+const age = (thisYear - birthYear);
 
 
-const greeting = "Hello! My name is " + fullName + " and I am "+ age + " years old."
+const greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
 
+console.log(greeting)
 
 
 // Do not change this
