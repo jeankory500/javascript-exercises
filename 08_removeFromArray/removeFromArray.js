5977cda5f0f0fd817c0d11aef84c16227d215659
JeankoryV <124745650+jeankory500@users.noreply.github.com>
2025-11-30 17:05:22 -0500

    nov30 Update

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args){
+    const newResult = [];
+
+    array.forEach((item)=> {
+        if(!args.includes(item)){
+            newResult.push(item)
+        }
+    })
+    return newResult;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0 ) return 'ERROR';
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+
+    /* Modern more simple way to right that would be
+    [min, max] = [max, min];
+    */
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum
+
+    
+    
 
 };
 
