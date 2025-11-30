d5ff201ddaae2566d90d5f288ce72a3afb56c0f2
JeankoryV <124745650+jeankory500@users.noreply.github.com>
2025-11-30 18:34:10 -0500

    last commit of the day

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(Fahrenheit) {
+  let Celsius = (Fahrenheit - 32)* 5/9;
+    return Math.round(Celsius * 10) / 10;
+};  
 
-const convertToFahrenheit = function() {
-};
+/*
+ let Celsius = (Fahrenheit * 9/5 + 32);
+    return Math.round(Celsius * 10) / 10;
+*/
+const convertToFahrenheit = function(Celsius) {
+  let Fahrenheit = (Celsius * 9/5 + 32);
+    return Math.round(Fahrenheit * 10) / 10;
+};  
+
+/*
+let Fahrenheit = (Celsius - 32) * 5/9;
+    return Math.round(Fahrenheit * 10) / 10;
+*/
 
 // Do not edit below this line
 module.exports = {
