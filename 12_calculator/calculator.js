b5b115059bc3943c4afbb78de9350da889e65021
JeankoryV <124745650+jeankory500@users.noreply.github.com>
2025-12-02 21:07:40 -0500

    dec2 Update

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function(num1,num2) {
+	return num1 + num2;
 
 };
 
-const power = function() {
+const subtract = function(num1,num2) {
+  return num1 - num2;
 	
 };
 
-const factorial = function() {
+const sum = function(arr) {
+  let total = 0;
+  for (i = 0; i < arr.length ;i++){
+    total += arr[i]
+  }
+	return total
+};
+
+const multiply = function(arr) {
+  let total = 1 
+  for (i=0; i< arr.length;i++){
+    total *= arr[i];
+  }
+return total;
+};
+
+const power = function(base, exponent) {
+  return Math.pow(base,exponent);
 	
+};
+
+const factorial = function(n) {
+  if (n < 0) return undefined
+  let result = 1
+  for (let i = 2; i <= n; i++){
+    result *= i;
+    
+  }
+	return result;
 };
 
 // Do not edit below this line
